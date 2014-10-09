d244ff7c27ce2ccedb3ec0498c7a83d252553c17
carvilsi <carvilsi.mail.list@gmail.com>
2014-10-09 16:27:50 +0200

    control botonera mediadte vista local

--- config/routes.js
+++ config/routes.js
@@ -22,36 +22,42 @@
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
+     * etc. depending on your default view engine) your home page.              *
+     *                                                                          *
+     * (Alternatively, remove this and add an `index.html` file in your         *
+     * `assets` directory)                                                      *
+     *                                                                          *
+     ***************************************************************************/
 
-  'get /': {
-    view: 'wapp'
-  },
-  'get /fichas': {
-	  view: 'fichas/'
-  },
-  'get /servicios': {
-	  view: 'servicios/'
-  },
-  'GET /servicio/todos':'ServicioController.buscaAhora',
-  'GET /servicio/elimina':'ServicioController.eliminaFicha'
+    'get /': {
+        view: 'wapp'
+    },
+    'get /fichas': {
+        view: 'fichas/',
+    },
+    'get /fichas/nueva': {
+        view: 'fichas/',
+        locals: {
+            layout: 'nueva'
+        }
+    },
+    'get /servicios': {
+        view: 'servicios/'
+    },
+    'GET /servicio/todos': 'ServicioController.buscaAhora',
+    'GET /servicio/elimina': 'ServicioController.eliminaFicha'
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  *  If a request to a URL doesn't match any of the custom routes above, it  *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Custom routes here...                                                    *
+     *                                                                          *
+     *  If a request to a URL doesn't match any of the custom routes above, it  *
+     * is matched against Sails route blueprints. See `config/blueprints.js`    *
+     * for configuration options and examples.                                  *
+     *                                                                          *
+     ***************************************************************************/
 
 };
